85482244e54132c404a86c5aebd49c8603112d42
Ben McClure <ben.mcclure@gmail.com>
2017-06-22 23:14:39 -0500

    Change function definition

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var exec = require("child_process").exec;
 
 module.exports = {
-    set_hostname: set_hostname
+    set_hostname: _set_hostname
 }
 
-set_hostname = function (hostname, callback) {
+function _set_hostname(hostname, callback) {
     exec("sudo hostnamectl set-hostname " + hostname, callback);
 };
