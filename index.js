fc952119f7fea0d6f12af98b0cbfdeba47e8feab
Ben McClure <ben.mcclure@gmail.com>
2017-06-24 22:19:04 -0500

    Add get_hostname export

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-var exec = require("child_process").exec;
+var exec = require("child_process").exec
+var os = require("os")
 
 module.exports = {
+    get_hostname: _get_hostname,
     set_hostname: _set_hostname
 }
 
+function _get_hostname(callback) {
+    callback(os.hostname())
+}
+
 function _set_hostname(hostname, callback) {
-    exec("sudo hostnamectl set-hostname " + hostname, callback);
-};
+    exec("sudo hostnamectl set-hostname " + hostname, callback)
+}
